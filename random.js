f399d3f5f34012bdff4f330ef467c4436ca02852
Bow Clearwater <rasamalagon@gmail.com>
2015-07-01 13:08:58 +0200

    random version 2

--- random.js
+++ random.js
@@ -1,8 +1,12 @@
 //Math.random() devueve numero aleatorio entre 0 y 1.
 var numero=Math.random();
 if(numero<=0.5){
-		console.log('\n'+numero+' menor que 0,5\n')
+		console.log('\n'+numero+' menor que 0,5\n\t'+randomnumber(numero)+'\n')
 		}
 else{
-	console.log('\n'+numero+' mayor que 0,5\n')
+	console.log('\n'+numero+' mayor que 0,5\n\t'+randomnumber(numero)+'\n')
 }
+function randomnumber(n){
+			n*=100;
+			return n.toFixed(2);
+		}
